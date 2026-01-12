759a2efcfd3dd2b8ce030bd130d7f8ee1cd0d315
sachinbudhamagar <sachinbmagar19@gmail.com>
2026-01-13 00:29:39 +0900

    Checking command-line argument in main function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 def get_github(username):
     """Fetch GitHub user data from API""" 
     try:
-        response = response.get("https://api.github.com/users/{username}")
+        response = requests.get("https://api.github.com/users/{username}")
         if response.status_code == 404: #404 == User Not Found
             return None
         elif response.status_code == 200: #200 == Found/OK
@@ -26,7 +26,7 @@ def validate(username):
     else:
         return True, username
     
-def get_interactive(username):
+def get_interactive():
     """Get username with validation and re-prompting"""
     while True:
         username = input("Enter Username: ")
@@ -39,3 +39,32 @@ def get_interactive(username):
             if retry != "y":
                 return None
 
+
+def main():
+    """Checking for command-line argument first"""
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+        is_valid, result = validate(username)
+        if not is_valid:
+            return f"Error: {result}"
+            sys.exit(1)
+        username = result
+    else:
+        username = get_interactive()
+        if username is None:
+            return f"Exiting..."
+            sys.exit(0)
+
+    print(f"Fetching data for '{username}'..")
+    user_data = get_github(username)
+
+    if user_data:
+        print(f"\n User found: {user_data['login']}")
+        print(f"Name: {user_data.get('name', 'N/A')}")
+        print(f"Public repos: {user_data.get('public_repos', 0)}")
+    else:
+        print(f"User '{username}' not found")
+        sys.exit(1)
+
+if __name__ =="__main__":
+    main()
